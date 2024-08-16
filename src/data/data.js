b8a8a8a9f051fe0fac83280fed17a227b580ea21
MickhOse-Zitrop <mickhose-zitrop@yandex.ru>
2024-08-16 17:13:10 +0300

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -32,37 +32,37 @@ export const videos = [{
     text: 'The new video is already on the channel! In this video we show our weekend. We hope for your support (well, likes and subscriptions 👍), and meet new content very soon!'
 }, {
     id: 'https://www.youtube.com/watch?v=X4OyaWqCLoE&t',
-    image: 'mazbistemfik/images/video7.jpg',
+    image: '/mazbistemfik/images/video7.jpg',
     title: 'VLOG: A new person has come to us',
     text: 'And here\'s a new video! And the one you\'ve never heard of and, of course, whom you haven\'t seen yet came to us. It is in this video that you will learn about him and how we spent time. After viewing, you can like and subscribe :)'
 }, {
     id: 'https://www.youtube.com/watch?v=sBhrlldn5OY&t',
-    image: '/images/video6.jpg',
+    image: '/mazbistemfik/images/video6.jpg',
     title: 'War of Factions: Mazbistemfik VS NGA',
     text: 'It\'s a new video. There we showed our war with our rival - NGA. If you like realistic fight, you should watch it! Of course, you could subscribe on us & like this and other movies.'
 }, {
     id: 'https://www.youtube.com/watch?v=HVuA9P6lGFo&t',
-    image: '/images/video5.jpg',
+    image: '/mazbistemfik/images/video5.jpg',
     title: 'VLOG: Cycling and rowing competitions',
     text: 'After quite a long time, he still appeared. The video turned out very big and interesting. Your activity accelerates output of new videos) Enjoy watching!'
 }, {
     id: 'https://www.youtube.com/watch?v=Y626ay5yMm8&t',
-    image: '/images/video4.jpg',
+    image: '/mazbistemfik/images/video4.jpg',
     title: 'The first days of the holidays',
     text: 'The new video has already arrived! And we started spring break. Our the solution is to show it to you. So we present this video is for your attention. You can like and subscribe below the video. Have a nice time!'
 }, {
     id: 'https://www.youtube.com/watch?v=YeFEo3zZfAU&t',
-    image: '/images/video3.jpg',
+    image: '/mazbistemfik/images/video3.jpg',
     title: 'Part 1: "Breaking into an unknown basement"',
     text: 'Description of the new video: 3 years ago, MickhOse Zitrop and Adeyl decided just to see what is in the basement, and we saw something there. We decided to find out what was there again.',
 }, {
     id: 'https://www.youtube.com/watch?v=gzz4yjKDVnQ&t',
-    image: '/images/video2.jpg',
+    image: '/mazbistemfik/images/video2.jpg',
     title: 'An overview of our USUAL day when it snows',
     text: 'We live in Crimea, and it rarely snows, especially so much. So we decided to shoot what we do in this weather. Enjoy watching!!!'
 }, {
     id: 'https://www.youtube.com/watch?v=Za1s2AiulZU&t',
-    image: '/images/video1.jpg',
+    image: '/mazbistemfik/images/video1.jpg',
     title: 'A short film about the Great Patriotic War',
     text: 'This is a short film about the Great Patriotic War. In this case the video features a fictional battle of the Red Army and fascist invaders. The Red Army needs to find a letter to Hitler about the location of the German troops, and troops of the USSR wins the letter.'
 }, ]
@@ -70,54 +70,54 @@ export const videos = [{
 export const founders = [{
     id: 'adeyl',
     title: 'Adeyl',
-    mainPhoto: '/images/adeyl.jpg'
+    mainPhoto: '/mazbistemfik/images/adeyl.jpg'
 }, {
     id: 'big-sloune',
     title: 'Big Sloune',
-    mainPhoto: '/images/big-sloune.jpg'
+    mainPhoto: '/mazbistemfik/images/big-sloune.jpg'
 }, {
     id: 'kary',
     title: 'Kary',
-    mainPhoto: '/images/kary.png',
+    mainPhoto: '/mazbistemfik/images/kary.png',
     not: true
 }, {
     id: 'mickhose-zitrop',
     title: 'MickhOse Zitrop',
-    mainPhoto: '/images/mickhose-zitrop.jpg'
+    mainPhoto: '/mazbistemfik/images/mickhose-zitrop.jpg'
 }, {
     id: 'mort-flame',
     title: 'Mort Flame',
-    mainPhoto: '/images/mort-flame.jpg'
+    mainPhoto: '/mazbistemfik/images/mort-flame.jpg'
 }, {
     id: 'tinaki',
     title: 'TiNaki',
-    mainPhoto: '/images/tinaki.jpg'
+    mainPhoto: '/mazbistemfik/images/tinaki.jpg'
 }, ]
 
 
 export const members = [{
     id: 'adeyl',
     title: 'Adeyl',
-    mainPhoto: '/images/adeyl.jpg'
+    mainPhoto: '/mazbistemfik/images/adeyl.jpg'
 }, {
     id: 'big-sloune',
     title: 'Big Sloune',
-    mainPhoto: '/images/big-sloune.jpg'
+    mainPhoto: '/mazbistemfik/images/big-sloune.jpg'
 }, {
     id: 'kary',
     title: 'Kary',
-    mainPhoto: '/images/kary.png',
+    mainPhoto: '/mazbistemfik/images/kary.png',
     not: true
 }, {
     id: 'mickhose-zitrop',
     title: 'MickhOse Zitrop',
-    mainPhoto: '/images/mickhose-zitrop.jpg'
+    mainPhoto: '/mazbistemfik/images/mickhose-zitrop.jpg'
 }, {
     id: 'mort-flame',
     title: 'Mort Flame',
-    mainPhoto: '/images/mort-flame.jpg'
+    mainPhoto: '/mazbistemfik/images/mort-flame.jpg'
 }, {
     id: 'tinaki',
     title: 'TiNaki',
-    mainPhoto: '/images/tinaki.jpg'
+    mainPhoto: '/mazbistemfik/images/tinaki.jpg'
 }, ]
